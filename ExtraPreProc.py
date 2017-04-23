bdf015d90ae94e03752b04aab2607d57ba176912
Shyamal Vaderia <vaderiashyamal@gmail.com>
2017-04-23 16:00:30 +0530

    ExtraPreProc added
    This file contains stem function, remove_stop word function, remove_punctuation function

---
+++ ExtraPreProc.py
@@ -0,0 +1,34 @@
+import os,csv,re
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.snowball import SnowballStemmer
+
+
+def remove_punctuations(text_string):
+        #defining a list of special characters to be used for text cleaning
+        special_characters = [",",".","'",";","\n", "?", "!", ":", ")", "(", "@", "*", "{", "}", "#",":", "_", "+", "`", "~", "$", "%", "^", "&", "","<",">","=","`","\"","'"] 
+        cleaned_string = str(text_string)
+        # removing special character
+        for ch in special_characters:
+            cleaned_string = cleaned_string.replace(ch, "")
+            cleaned_string = cleaned_string.lower()
+        return cleaned_string
+
+def remove_stop_words(document):
+        pwd = os.getcwd()
+        words_file = pwd + "/stop_words.txt"
+        stop_word_list = []
+        stop_word_list = [word for line in open(words_file, 'r') for word in line.split(",")]
+        cleaned_doc = []
+        for term in document.split(" "):
+            term = remove_punctuations(term)
+            if term not in stop_word_list:
+                cleaned_doc.append(term)
+        return cleaned_doc
+
+def stem(tweet):
+    tweet = tweet.split()
+    wnl = WordNetLemmatizer()
+    tweet = [wnl.lemmatize(w) for w in tweet]
+    stemmer = SnowballStemmer("english",ignore_stopwords=True)
+    tweet = [stemmer.stem(w) for w in tweet]
+    return " ".join(tweet)
