1a161c771eed7a95181b1525448d466434adb0a7
Shyamal Vaderia <vaderiashyamal@gmail.com>
2017-04-23 15:58:13 +0530

    Update in feature2
    added syllables.py bcz previous version of code containing cmudict was not working for words that are not in it's dictionnary.
    even 'Facebook' 'Twitter' 'instagram' are not there in cmudict.

--- feature2-word-length-distribution.py
+++ feature2-word-length-distribution.py
@@ -2,51 +2,36 @@ import numpy
 import curses
 from curses.ascii import isdigit
 import nltk
-from nltk.corpus import cmudict
+from syllables import *
+from collection import Counter
 
-
-d=cmudict.dict()
-
-def nsyl(word):
-    return [len(list(y for y in x if isdigit(y[-1]))) for x in d[word.lower()]]
 
 def feature_syllables(tweet):
-    i=0
-    j=0
-    k=0
-    ls=tweet.split(" ")
-    for word in ls:
-        i+=1
-        k+=nsyl(word)[0]
-        if k>2:
-            j+=1
+    words=0
+    pollysyllable=0
+    syllable=0
+    for word in tweet:
+        words+=1
+        syllable += sylco(word)
+        if syllable>2:
+            pollysyllable+=1
         
-    return [i,j,k,j/i,k/i]
-
-#print(feature_syllables('How you doing'))
+    return [words, pollysyllable, syllable, pollysyllable/words, syllable/words]
 
 def mode(lda):
-    maxkey=1
-    max=0
-    for i in range(20):
-        if lda[i]>max:
-            maxkey=i
-    return maxkey
+    data = Counter(lda)
+    return data.most_common(1)[0][0]
 
 def word_length_distribution(tweet):
-    wordlist=tweet.split(" ")
-    lda=[0 for x in range(0,20)]
-
-    for i in wordlist:
-        if(len(i))<=20:
-            lda[len(i)-1]+=1
+    lda=[0]*19
+    for word in tweet:
+        if(len(word))<20:
+            lda[len(word)-1]+=1
     arr=[]
-    for i in range(20):
-        for j in range(lda[i]):
-            if lda[i]>0:
-                arr.append(i)
-
-    return (numpy.average(arr), numpy.median(arr),mode(lda), numpy.std(arr), numpy.amax(lda), numpy.amin(lda))
+    for i in range(19):
+        if lda[i]>0:
+           arr += [i+1]*lda[i]
+    return (numpy.average(arr), numpy.median(arr), mode(lda), numpy.std(arr), max(lda), min(lda))
 
 def JS(d1,d2):
     mean = [(float(x)+ float(y))/2 for x,y in zip(d1, d2)]
@@ -55,14 +40,4 @@ def JS(d1,d2):
     return float(0.5*div1) + float(0.5*div2)
 
 def KL(t1,t2):
-    sum=0
-    for i in range(len(t1)):
-        x = float(t1[i])/float(t2[i])
-        if x >0:
-          value = math.log(x)*t1[i]
-        else:
-          value =0
-        sum =sum+value
-    return sum
-
-
+    return sum([t1[i] * math.log(t1[i]/t2[i]) for i in range(len(t1)) if t1[i] != 0 if t2[i] != 0])
