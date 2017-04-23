2874aa36dec4471eb94c634ff16192d8f8f03e4b
Shyamal Vaderia <vaderiashyamal@gmail.com>
2017-04-23 13:30:56 +0530

    Update in Preproc

--- PreProc.py
+++ PreProc.py
@@ -1,18 +1,15 @@
 import re
-from nltk import word_tokenize
-from nltk.stem import WordNetLemmatizer
 import csv
 import os
 
 
 def main():
-    pwd = os.getcwd()
+    pwd = os.getcwd();
     norm_in = pwd + "/normal_with_past"
     norm_out = pwd + "/normal_with_past_PP"
     sarc_in = pwd + "/sarcastic_with_past"
     sarc_out = pwd + "/sarcastic_with_past_PP"
-    stop = set(stopwords.words('english'))
-
+    
     for f in os.listdir(norm_in):
         inputFile = open(os.path.join(norm_in,f),"r")
         outputFile = open(os.path.join(norm_out,f),"w")
@@ -36,20 +33,11 @@ def main():
         inputFile.close()
         outputFile.close()
 
-    # inputFile = open("test_data.csv", "r")
-    # outputFile = open("processed.csv", "w")
-    # reader = csv.reader(inputFile)
-    # writer = csv.writer(outputFile)
-    # for row in reader:
-    #     row[2] = preprocess(row[2])
-    #     # print row[2]
-    #     writer.writerow(row) 
-
 def remove_punctuations(text_string):
         #defining a list of special characters to be used for text cleaning
-        special_characters = [",",".","'",";","\n", "?", "!", ":", ")", "(", "@", "*", "{", "}", "#",":", "_", "+", "`", "~", "$", "%", "^", "&", ""] 
+        special_characters = [",",".","'",";","\n", "?", "!", ":", ")", "(", "@", "*", "{", "}", "#",":", "_", "+", "`", "~", "$", "%", "^", "&", "","<",">","=","`","\"","'"] 
         cleaned_string = str(text_string)
-        # removing stop words
+        # removing special character
         for ch in special_characters:
             cleaned_string = cleaned_string.replace(ch, "")
             cleaned_string = cleaned_string.lower()
@@ -62,7 +50,6 @@ def remove_stop_words(document):
         stop_word_list = [word for line in open(words_file, 'r') for word in line.split(",")]
         cleaned_doc = []
         for term in document.split(" "):
-            # print term
             term = remove_punctuations(term)
             if term not in stop_word_list:
                 cleaned_doc.append(term)
@@ -72,8 +59,8 @@ def preprocess(tweet):
     tweet = tweet.replace("#sarcasm","")                                                    #Removes the sarcasm hashtag
     tweet = tweet.replace("#sarcastic","")
     tweet = tweet.replace("#not","")
-    tweet = re.sub(r"(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9]+)", "", tweet) 
-    tweet = re.sub(r'(https?|ftp)://[^\s/$.?#].[^\s]*', '', tweet, flags=re.MULTILINE)      #Removes URL                                                   
+    tweet = re.sub(r"(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9]+)", "", tweet)      #Removes mentions
+    tweet = re.sub(r'(https?|ftp)://[^\s/$.?#].[^\s]*', '', tweet, flags=re.MULTILINE)      #Removes URL        
     tweet = remove_stop_words(tweet)                                   
     tweet = " ".join(tweet)
     return tweet
@@ -81,4 +68,3 @@ def preprocess(tweet):
 
 if __name__=="__main__":
     main()
-
