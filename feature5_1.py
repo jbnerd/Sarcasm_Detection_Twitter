9f3aecfa4be7324b02f19995274e92e46e6cfa0d
Shyamal Vaderia <vaderiashyamal@gmail.com>
2017-04-24 18:59:02 +0530

    Feature5_1 by Abhishek is done! :smile:

--- feature5_1.py
+++ feature5_1.py
@@ -2,48 +2,51 @@ import csv
 import os
 
 pwd = os.getcwd()
-norm_input = pwd + "/normal_with_past"
-sarc_input = pwd + "/sarcastic_with_past"
+norm_input = pwd + "/normal_with_past_PP"
+sarc_input = pwd + "/sarcastic_with_past_PP"
+csvfile = open("feature5_1.csv","w")
+f5 = csv.writer(csvfile,delimiter=",")
 
-result = []
 
-for f in os.listdir(norm_input):
+for f in sorted(os.listdir(norm_input)):
 	input_file = open(os.path.join(norm_input, f), "r")
 	reader = csv.reader(input_file)
-	temp_res = []
-	for row in reader:
-		required = row[2]
-		letters = []
-		for i in required:
-			letters.append(i)
-		tot_letts = len(letters)
-		letters = list(set(letters))
-		if(tot_letts != 0):
-			prob = len(letters)/tot_letts
-		temp = []
-		temp.append(prob)
-		temp.append(tot_letts)
-		temp_res.append(temp)
-	result.append(temp_res)
+	reader = list(reader)
+	required = reader[1][2]
+	letters = required.split(" ")
+	let = "".join(letters)
+	letters = []
+	for c in let:
+		letters.append(c)
+	tot_letts = len(letters)
+	letters = list(set(letters))
+	if(tot_letts != 0):
+		prob = len(letters)/tot_letts
+	temp = []
+	temp.append(prob)
+	temp.append(tot_letts)
+	f5.writerow(temp)
+	input_file.close()
 
-
-for f in os.listdir(sarc_input):
+for f in sorted(os.listdir(sarc_input)):
 	input_file = open(os.path.join(sarc_input, f), "r")
 	reader = csv.reader(input_file)
-	temp_res = []
-	for row in reader:
-		letters = []
-		required = row[2]
-		letters = []
-		for i in required:
-			letters.append(i)
-		tot_letts = len(letters)
-		letters = list(set(letters))
-		if(tot_letts != 0):
-			prob = len(letters)/tot_letts
-		temp = []
-		temp.append(prob)
-		temp.append(tot_letts)
-		temp_res.append(temp)
-	result.append(temp_res)
-print(result)
+	reader = list(reader)
+	required = reader[1][2]
+	letters = required.split(" ")
+	letters = "".join(letters)
+	let = "".join(letters)
+	letters = []
+	for c in let:
+		letters.append(c)
+	tot_letts = len(letters)
+	letters = list(set(letters))
+	if(tot_letts != 0):
+		prob = len(letters)/tot_letts
+	temp = []
+	temp.append(prob)
+	temp.append(tot_letts)
+	f5.writerow(temp)
+	input_file.close()
+
+csvfile.close()
